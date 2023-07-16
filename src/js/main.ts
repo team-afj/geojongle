2a28f8e6da8506b0280f64b12dc867b598a4330b
Ulysse Gérard <thevoodoos@gmail.com>
2023-07-16 23:01:06 +0200

    Add a locate button

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,7 +4,10 @@ import { Address, addresses } from "./data";
 
 Leaflet.Icon.Default.imagePath = "images/";
 
+// Page elements
 const table = document.getElementById("data-tbl")! as HTMLTableElement;
+
+// Map initialization
 
 let map = Leaflet.map("map").setView([46.603354, 1.8883335], 5);
 
@@ -14,12 +17,40 @@ Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+const locate_svg =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M3.05,13H1V11H3.05C3.5,6.83 6.83,3.5 11,3.05V1H13V3.05C17.17,3.5 20.5,6.83 20.95,11H23V13H20.95C20.5,17.17 17.17,20.5 13,20.95V23H11V20.95C6.83,20.5 3.5,17.17 3.05,13M12,5A7,7 0 0,0 5,12A7,7 0 0,0 12,19A7,7 0 0,0 19,12A7,7 0 0,0 12,5Z" /></svg>';
+
+// Controls
+const locate_btn = Leaflet.DomUtil.create("div");
+locate_btn.classList.add("leaflet-bar");
+let locate_btn_link = Leaflet.DomUtil.create("a");
+locate_btn_link.classList.add("locate-icon");
+locate_btn.appendChild(locate_btn_link);
+
+let Locate = Leaflet.Control.extend({
+  onAdd: function (map) {
+    Leaflet.DomEvent.on(locate_btn_link, "click", (_) =>
+      map.locate({ setView: true, maxZoom: 12 }),
+    );
+
+    return locate_btn;
+  },
+
+  onRemove: function (map) {
+    Leaflet.DomEvent.off(locate_btn_link);
+  },
+});
+
+map.addControl(new Locate({ position: "topleft" }));
+
+// Markers
+
 const make_marker = (address: Address) => {
   let m = Leaflet.marker([address.osm_data.lat, address.osm_data.lon]).addTo(
-    map
+    map,
   );
   m.bindPopup(
-    `<b>${address.name}</b></br><a target="_blank" href="${address.url}">Voir les informations les plus récentes.</a><br>${address.description}`
+    `<b>${address.name}</b></br><a target="_blank" href="${address.url}">Voir les informations les plus récentes.</a><br>${address.description}`,
   ).openPopup();
   return m;
 };
