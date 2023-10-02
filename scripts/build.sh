b23b8ac6905979b5da07b8bfc65634cf15eadcc2
Ulysse Gérard <thevoodoos@gmail.com>
2023-10-02 20:33:24 +0200

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 cp -r "node_modules/leaflet/dist/images" dist/leaflet-images
 
 yarn exec -- esbuild $1 --bundle --minify --outdir=dist \
-    --loader:.png=empty --allow-overwrite src/js/main.ts
+    "--external:*.png" --allow-overwrite src/js/main.ts
